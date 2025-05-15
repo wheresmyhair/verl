0343f9faa2c88ca5fb8e896241e767bde96c6b2a
YizhenJia <yizhen.jia96@gmail.com>
2025-05-15 10:55:34 +0800

    ppo tune

--- examples/repro/run_ppo.sh
+++ examples/repro/run_ppo.sh
@@ -29,7 +29,7 @@ python3 -m verl.trainer.main_ppo \
     data.filter_overlong_prompts=True \
     data.truncation='error' \
     data.return_raw_chat=True \
-    actor_rollout_ref.model.path="Qwen/Qwen2.5-7B-Instruct" \
+    actor_rollout_ref.model.path="Qwen/Qwen2.5-3B-Instruct" \
     actor_rollout_ref.actor.optim.lr=1e-6 \
     actor_rollout_ref.model.use_remove_padding=True \
     actor_rollout_ref.actor.optim.lr_warmup_steps_ratio=0.1 \
