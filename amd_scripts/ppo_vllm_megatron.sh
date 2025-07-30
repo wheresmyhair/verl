8a9630109827f66bd9d0470eba3be3aa38050998
YizhenJia <yizhen.jia96@gmail.com>
2025-07-30 17:11:59 +0800

    try larger tp

--- amd_scripts/ppo_vllm_megatron.sh
+++ amd_scripts/ppo_vllm_megatron.sh
@@ -83,6 +83,7 @@ PYTHONUNBUFFERED=1 python3 -m verl.trainer.main_ppo --config-path=/root/verl/amd
     actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=4 \
     actor_rollout_ref.rollout.log_prob_micro_batch_size_per_gpu=8 \
     actor_rollout_ref.rollout.tensor_model_parallel_size=1 \
+    actor_rollout_ref.actor.megatron.tensor_model_parallel_size=4 \
     actor_rollout_ref.rollout.name=$ENGINE \
     actor_rollout_ref.rollout.gpu_memory_utilization=0.8 \
     actor_rollout_ref.ref.log_prob_micro_batch_size_per_gpu=4 \
