28a07ba1c128b177c05967088e5fc372674b9724
YizhenJia <yizhen.jia96@gmail.com>
2025-08-26 14:24:19 +0800

    update cfg home dir

--- amd_scripts/comp_grpo_vllm_megatron.sh
+++ amd_scripts/comp_grpo_vllm_megatron.sh
@@ -3,9 +3,8 @@ set -x
 
 export HIP_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 export RAY_EXPERIMENTAL_NOSET_HIP_VISIBLE_DEVICES=1
-export HF_HOME=/home/johnny/hf_cache
 
-python3 examples/data_preprocess/gsm8k.py --local_dir ~/data/gsm8k
+python3 examples/data_preprocess/gsm8k.py --local_dir $HOME/data/gsm8k
 
 gsm8k_train_path=$HOME/data/gsm8k/train.parquet
 gsm8k_test_path=$HOME/data/gsm8k/test.parquet
@@ -24,7 +23,7 @@ GPU_MEMORY_UTILIZATION=0.6 #If deepseek, set GPU_MEMORY_UTILIZATION=0.6
 
 YOUR_RUN_NAME=$ENGINE-TP$TP_VALUE-BSZ$INFERENCE_BATCH_SIZE-GMEM$GPU_MEMORY_UTILIZATION
 
-PYTHONUNBUFFERED=1 python3 -m verl.trainer.main_ppo --config-path=/root/verl/amd_scripts/config \
+PYTHONUNBUFFERED=1 python3 -m verl.trainer.main_ppo --config-path=$HOME/verl/amd_scripts/config \
     --config-name='ppo_megatron_example.yaml' \
 	algorithm.adv_estimator=grpo \
 	data.train_files=$train_files \
--- amd_scripts/run_docker.sh
+++ amd_scripts/run_docker.sh
@@ -1,6 +1,6 @@
-docker pull yushengsuthu/verl:verl-0.4.1_ubuntu-22.04_rocm6.3.4-numa-patch_vllm0.8.5_sglang0.4.6.post4
+sudo docker pull yushengsuthu/verl:verl-0.4.1_ubuntu-22.04_rocm6.3.4-numa-patch_vllm0.8.5_sglang0.4.6.post4
 
-docker tag yushengsuthu/verl:verl-0.4.1_ubuntu-22.04_rocm6.3.4-numa-patch_vllm0.8.5_sglang0.4.6.post4 verl-rocm:latest
+sudo docker tag yushengsuthu/verl:verl-0.4.1_ubuntu-22.04_rocm6.3.4-numa-patch_vllm0.8.5_sglang0.4.6.post4 verl-rocm:latest
 
 cd $HOME
 
@@ -8,7 +8,7 @@ git clone https://github.com/wheresmyhair/verl.git && cd verl
 
 git checkout amd-0.4.1
 
-docker run --rm -it \
+sudo docker run --rm -it \
   --device /dev/dri \
   --device /dev/kfd \
   -p 8265:8265 \
@@ -16,7 +16,9 @@ docker run --rm -it \
   --security-opt seccomp=unconfined \
   --privileged \
   -v $HOME/.ssh:/root/.ssh \
-  -v $HOME/eric:$HOME \
+  -v $HOME:$HOME \
+  -e HOME=$HOME/eric \
+  -e HF_HOME=$HOME/eric/hf_cache \
   --shm-size 128G \
   verl-rocm \
   /bin/bash
