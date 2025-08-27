f965e1927dd4dbe22c6b1713c9c4d02087118873
YizhenJia <yizhen.jia96@gmail.com>
2025-08-27 16:09:45 +0800

    typo fix

--- amd_scripts/comp_grpo_vllm_megatron.sh
+++ amd_scripts/comp_grpo_vllm_megatron.sh
@@ -46,8 +46,8 @@ python3 -m verl.trainer.main_ppo --config-path=./config --config-name='ppo_megat
 	actor_rollout_ref.actor.kl_loss_type=low_var_kl \
 	actor_rollout_ref.actor.profiler.enable=True \
 	actor_rollout_ref.actor.profiler.all_ranks=True \
-	actor_rollout_ref.model.profiler.tool_config.torch.step_start=0 \
-	actor_rollout_ref.model.profiler.tool_config.torch.step_end=9 \
+	actor_rollout_ref.actor.profiler.tool_config.torch.step_start=0 \
+	actor_rollout_ref.actor.profiler.tool_config.torch.step_end=9 \
 	actor_rollout_ref.actor.megatron.tensor_model_parallel_size=$TRAIN_TP \
 	actor_rollout_ref.actor.megatron.pipeline_model_parallel_size=$TRAIN_PP \
 	actor_rollout_ref.rollout.log_prob_micro_batch_size_per_gpu=$INFERENCE_BATCH_SIZE \
